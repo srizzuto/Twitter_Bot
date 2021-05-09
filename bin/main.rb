e24b790d2eaf7557f63c9ef36522c75937d727d3
Sebastian Rizzuto <srizzuto@gmail.com>
2021-05-08 21:16:51 -0300

    add retweeter

--- bin/main.rb
+++ bin/main.rb
@@ -1,22 +1,30 @@
 #!/usr/bin/env ruby
 
 require 'twitter'
+require './lib/news_retweet.rb'
+
+#$client = nil
+#$hashtag = "#argentina"
 
 class TeiroBot
+  attr_accessor :client, :hashtag
+  CONSUMER_KEY = '99P4HngMjK4rml2SDLlCia2J0'.freeze
+  CONSUMER_SECRET = 'A3sVY2Md5AauDIGyoOMbv3vz39l5Ng5ZTdOlL7xhrItuuFIKCR'.freeze
 
-  CONSUMER_KEY = '99P4HngMjK4rml2SDLlCia2J0'
-  CONSUMER_SECRET = 'A3sVY2Md5AauDIGyoOMbv3vz39l5Ng5ZTdOlL7xhrItuuFIKCR'
+  OA_TOKEN = '1390355577189113857-XZxk1xCqVOGhYZC6xxsz6bGX9GcGfd'.freeze
+  OA_SECRET = 'yvemotmgGQRsqfzvJU0YQbNCbtSuKuRBlgvFhCqfmqhkI'.freeze
 
-  OA_TOKEN = '1390355577189113857-XZxk1xCqVOGhYZC6xxsz6bGX9GcGfd'
-  OA_SECRET = 'yvemotmgGQRsqfzvJU0YQbNCbtSuKuRBlgvFhCqfmqhkI'
-
-  client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = CONSUMER_KEY
-    config.consumer_secret     = CONSUMER_SECRET
-    config.access_token        = OA_TOKEN
-    config.access_token_secret = OA_SECRET
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = CONSUMER_KEY
+      config.consumer_secret = CONSUMER_SECRET
+      config.access_token = OA_TOKEN
+      config.access_token_secret = OA_SECRET
+    end  
   end
-
-  client.update("Hello everyone")
-
 end
+
+TBot = TeiroBot.new()
+News.new().news_retweet(TBot.client, "#argentina", 3)
+
+
