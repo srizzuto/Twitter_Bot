95302641cbcf3ac63a7fc97b76f6a18f58eb1c53
Sebastian Rizzuto <srizzuto@gmail.com>
2021-05-11 11:53:45 -0300

    fix rubocop

--- lib/mentions_ans.rb
+++ lib/mentions_ans.rb
@@ -15,7 +15,6 @@ class Answers
     count = File.foreach(@filename).inject(0) { |c, _line| c + 1 }
     return read_line(1) if tweet.include? 'How are you'
     return read_line(2) if tweet.include? '?'
-    return read_line(3) if tweet.include?('haha' || 'lol' || 'funny')
     return read_line(4) if tweet.include? 'Hello'
     return read_line(5) if tweet.include? 'bye'
     return read_line(6) if tweet.include? 'thank you'
